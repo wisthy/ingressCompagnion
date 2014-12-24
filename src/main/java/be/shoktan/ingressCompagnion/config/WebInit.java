5a6e33f7614f5896262faa6bd9a34017e68ba747
wisthler <wisthler@gmail.com>
2014-12-24 13:24:37 +0100

    a basic working view with Thymleaf

--- src/main/java/be/shoktan/ingressCompagnion/config/WebInit.java
+++ src/main/java/be/shoktan/ingressCompagnion/config/WebInit.java
@@ -1,32 +1,40 @@
 package be.shoktan.ingressCompagnion.config;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-public class WebInit extends AbstractAnnotationConfigDispatcherServletInitializer {
-	/*
-	 * (non-Javadoc)
-	 * @see org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer#getRootConfigClasses()
-	 */
+//public class WebInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+//	/*
+//	 * (non-Javadoc)
+//	 * @see org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer#getRootConfigClasses()
+//	 */
+//	@Override
+//	protected Class<?>[] getRootConfigClasses() {
+//		return new Class<?>[] { RootConfig.class };
+//	}
+//	
+//	/*
+//	 * (non-Javadoc)
+//	 * @see org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer#getServletConfigClasses()
+//	 */
+//	@Override
+//	protected Class<?>[] getServletConfigClasses() {
+//		return new Class<?>[] { WebConfig.class };
+//	}
+//	
+//	/*
+//	 * (non-Javadoc)
+//	 * @see org.springframework.web.servlet.support.AbstractDispatcherServletInitializer#getServletMappings()
+//	 */
+//	@Override
+//	protected String[] getServletMappings() {
+//		return new String[] { "/" };
+//	}
+//}
+
+public class WebInit extends SpringBootServletInitializer {
 	@Override
-	protected Class<?>[] getRootConfigClasses() {
-		return new Class<?>[] { RootConfig.class };
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer#getServletConfigClasses()
-	 */
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		return new Class<?>[] { WebConfig.class };
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.springframework.web.servlet.support.AbstractDispatcherServletInitializer#getServletMappings()
-	 */
-	@Override
-	protected String[] getServletMappings() {
-		return new String[] { "/" };
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 }
--- src/main/java/be/shoktan/ingressCompagnion/web/HomeController.java
+++ src/main/java/be/shoktan/ingressCompagnion/web/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Controller in charge for base actions
@@ -16,5 +17,16 @@ public class HomeController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String home(Model model) {
 		return "home";
+	}
+	
+	@RequestMapping(value="stat", method = RequestMethod.GET)
+	public String stat(){
+		return "stat";
+	}
+	
+	@RequestMapping("properties")
+	@ResponseBody
+	public java.util.Properties properties() {
+		return System.getProperties();
 	}
 }
